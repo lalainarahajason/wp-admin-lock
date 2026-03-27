631700eb7fabeb1446da3132f7bf8c1430bc5730
Rahajason <lalaina@rahajason.com>
2026-03-27 22:50:01 +0100

    build(assets): update compiled assets for new Settings and Dashboard integration

--- plugin/build/index.asset.php
+++ plugin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2f9644b6cf0fe6f63770');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8f03902e794659a9917e');
