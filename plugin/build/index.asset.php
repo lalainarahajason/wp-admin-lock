378402c4aa45c9a1526bf652c7c492b9e34fa6c9
Rahajason <lalaina@rahajason.com>
2026-03-29 23:03:15 +0200

    build: update compiled assets after audit log changes

--- plugin/build/index.asset.php
+++ plugin/build/index.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n' ),
-	'version'      => '5d5a3181ad1dfc0ce213',
-);
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '192ded74401550ad5921');
